54791d4d968fafdeb390145d1139962efdf54903
raundalrahul <rahulraundal3@gmail.com>
2024-03-05 00:55:19 +0530

    added multer to the project

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,7 +1,29 @@
 import {v2 as cloudinary} from 'cloudinary';
-          
+import fs from 'fs';
+
 cloudinary.config({ 
-  cloud_name: 'dc0eqpxvn', 
-  api_key: '467751314988552', 
-  api_secret: 'R_3878Ims4ODYREj1EMKJOyRqe8' 
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+  api_key: process.env.CLOUDINARY_API_KEY, 
+  api_secret: process.env.CLOUDINARY_API_SECRET_KEY 
 });
+
+const uplodCloudinary = async (localFilePath)=>{
+  try{
+    if(!localFilePath) return null
+
+    //uplod cloudinary file path
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type:"auto"
+    })
+    //file has been uploded successfully.
+    console.log("The file has been uploded successfully", response.url);
+
+    return response
+  }catch (error){
+      fs.unlinkSync(localFilePath) //this will remove temp file if petation goes failed.
+
+      return null
+  }
+}
+
+export {uplodCloudinary}
