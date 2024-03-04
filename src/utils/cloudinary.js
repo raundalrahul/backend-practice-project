b98371a544726d62b56a1d2eaa2c4728c89ecaca
raundalrahul <rahulraundal3@gmail.com>
2024-03-04 20:20:22 +0530

    cloudinary added

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,7 @@
+import {v2 as cloudinary} from 'cloudinary';
+          
+cloudinary.config({ 
+  cloud_name: 'dc0eqpxvn', 
+  api_key: '467751314988552', 
+  api_secret: 'R_3878Ims4ODYREj1EMKJOyRqe8' 
+});
